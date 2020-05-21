38376843da17f28543fbb1295de4223408fa46f0
Jason <jason.w.g.stewart@gmail.com>
2020-05-21 08:26:54 +0100

    gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
-const gulp = require('gulp');
+const gulp = require("gulp"),
+  watch = require("gulp-watch");
 
-gulp.task('default', () => {
-    console.log('yeetboi')
-})
+gulp.task("default", () => {
+  console.log("I am a gulp task!");
+});
 
+gulp.task("html", () => {
+  console.log("this does something to html");
+});
+
+gulp.task("styles", () => {
+  console.log("this does something to css");
+});
+
+gulp.task("watch", () => {
+  watch("./app/index.html", () => {
+    gulp.start("html");
+  });
+
+  watch("./app/assets/styles/**/*.css", () => {
+    gulp.start("styles");
+  });
+});
